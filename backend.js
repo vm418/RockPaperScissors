fe59bebeddd2f5e78caf934a12a5c057d251bbe2
vm418 <vm418@cam.ac.uk>
2022-08-15 10:17:37 +0100

    backend complete

--- backend.js
+++ backend.js
@@ -1,52 +1,58 @@
+
+const body = document.body;
+const content = document.querySelector('.content');
+const buttons = document.querySelectorAll("button");
+// score boards for game
+const win = document.getElementById("win");
+const loss = document.getElementById("loss");
+// score counts
+let w = 0;
+let l = 0;
+
 function getComputerChoice(){
     // returns rock, paper or scissors randomly when called 
     let options = ["rock", "paper", "scissors"] ;
     return options[Math.floor( Math.random() * options.length )]       
 }
 
-function playRound (playerSelection, computerSelection) {
-    // allows a round of the game to be played
-    // make player input case insensitive
-    let player = playerSelection.toLowerCase();
-
+function playRound (player, computerSelection) {
+    // store result 
+    var result = "";
+    
     if (player === computerSelection){
-        return "It's a draw";
+        result =  "It's a draw";
     }else if ((player === "rock") && (computerSelection === "scissors")) {
-        return "You win! rock beats scissors";
+        result =  "You win! rock beats scissors";
     }else if ((player === "scissors") && (computerSelection === "paper")) {
-        return "You win! scissors beats paper";
+        result =  "You win! scissors beats paper";
     }else if ((player === "paper") && (computerSelection === "rock")) {
-        return "You win! paper beats scissors";
+        result = "You win! paper beats scissors";
     } else {
-        return "You lose! " + computerSelection + " beats " + player;
+        result ="You lose! " + computerSelection + " beats " + player;
+    }
+    // display the result
+    content.textContent = result;
+    // update score and display
+    if (result.includes("win")) {
+        w++;
+    }else if (result.includes("lose")){
+        l++;
+    }
+    win.textContent = w;
+    loss.textContent = l;
+    // once player or computer reaches 5 deactivates buttons
+    if(w==5 || l==5){
+        content.textContent="Game Over";
+        buttons.forEach(button => {
+            button.disabled = true
+
+        });
     }
 }
-function game (){
 
-    // function allows 5 games to be played and declares an overall winnner
-
-    let w = 0;
-    let l = 0;
-
-    for (let i = 0; i <5; i++){
-        let result = playRound(prompt("Your move....."), getComputerChoice());
-        console.log(result);
-        if (result.includes("win")) {
-            w++;
-        }else if (result.includes("lose")){
-            l++;
-        }
-    }
-
-    if (w > l){
-        console.log("You won the game!");
-    }else if (w < l){
-        console.log("You lost the game");
-    }else {
-        console.log("No one wins");
-    }
-
-
-}
-
-game();
+// if button is pressed play a round of the game
+buttons.forEach((button) => {
+    button.addEventListener("click", function(){
+        playRound(String(button.id),getComputerChoice())});
+        
+});
